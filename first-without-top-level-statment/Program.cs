233fcf39424da7160c3dad80d62095b34846f67f
Kevin Rodrigues <kevin.desenv@gmail.com>
2023-01-05 23:13:26 +0000

    adding first project

---
+++ first-without-top-level-statment/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace first_without_top_level_statment;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string nome = "Kevin";
+        string sobrenome = "Rodrigues";
+
+        char letraH = 'h';
+
+        int idade = 31;
+
+        double altura = 1.74;
+
+        bool estaVivo = true;
+
+        bool isRich = false;
+
+        Console.WriteLine(nome + " " + sobrenome);
+        Console.WriteLine($"{nome} {sobrenome}");
+
+        Console.WriteLine(letraH);
+
+        Console.WriteLine("Idade: " + idade);
+
+        Console.WriteLine("Altura: "+altura);
+
+        if(estaVivo){
+        Console.WriteLine("Está vivo"); 
+        } else{
+            Console.WriteLine("Não está vivo");
+        }
+
+        if(isRich){
+        Console.WriteLine("Essa pessoa é rica"); 
+        } else{
+            Console.WriteLine("Essa pessoa é pobre");
+        }
+    }
+}
---
+++ first/Program.cs
@@ -0,0 +1,39 @@
+﻿string nome = "Kevin";
+string sobrenome = "Rodrigues";
+
+char letraH = 'h';
+
+int idade = 31;
+
+double altura = 1.74;
+
+bool estaVivo = true;
+
+bool isRich = false;
+
+Console.WriteLine(nome + " " + sobrenome);
+
+Console.WriteLine(letraH);
+
+Console.WriteLine("Idade: " + idade);
+
+Console.WriteLine("Altura: "+altura);
+
+if(estaVivo){
+   Console.WriteLine("Está vivo"); 
+} else{
+    Console.WriteLine("Não está vivo");
+}
+
+if(isRich){
+   Console.WriteLine("Essa pessoa é rica"); 
+} else{
+    Console.WriteLine("Essa pessoa é pobre");
+}
+
+
+
+
+
+
+
